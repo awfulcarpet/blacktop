77decfe6d87d6914637f35544d70b7df41b444a7
awfulcarpet <161393599+awfulcarpet@users.noreply.github.com>
2025-04-09 08:40:02 -0600

    nuke yagsl

--- src/main/java/frc/robot/subsystems/Swerve.java
+++ src/main/java/frc/robot/subsystems/Swerve.java
@@ -7,58 +7,29 @@ package frc.robot.subsystems;
 import java.io.File;
 import java.util.function.Supplier;
 
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Rotation3d;
-import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
-import edu.wpi.first.wpilibj.RobotBase;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import swervelib.SwerveDrive;
-import swervelib.parser.SwerveParser;
-import swervelib.telemetry.SwerveDriveTelemetry;
-import swervelib.telemetry.SwerveDriveTelemetry.TelemetryVerbosity;
 
 public class Swerve extends SubsystemBase {
-	private final SwerveDrive swerve;
 	private final double maxSpeed = 5;
 
 	/** Creates a new Swerve. */
 	public Swerve(File dir) {
-		 SwerveDriveTelemetry.verbosity = TelemetryVerbosity.HIGH;
-		try {
-			swerve = new SwerveParser(dir).createSwerveDrive(maxSpeed);
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
-
-		swerve.resetOdometry(new Pose2d(5, 5, Rotation2d.kZero));
-
-		if (RobotBase.isSimulation()) {
-			swerve.setHeadingCorrection(false);
-			swerve.setCosineCompensator(false);
-		}
 	}
 
 	public void resetGyro() {
-		swerve.setGyro(Rotation3d.kZero);
-		swerve.resetOdometry(new Pose2d(5, 5, Rotation2d.kZero));
 	}
 
 	public Command drive(Supplier<Double> x, Supplier<Double> y, Supplier<Double> rotation, Supplier<Boolean> fieldRelative, Supplier<Boolean> openLoop) {
-		return run(() -> {
-			swerve.drive(new Translation2d(x.get(), y.get()), rotation.get(), fieldRelative.get(), openLoop.get());
-		});
+		return Commands.none();
 	}
 
 	public void fieldRelativeDrive(ChassisSpeeds vel) {
-		swerve.driveFieldOriented(vel);
 	}
 
 	@Override
 	public void periodic() {
-		SmartDashboard.putNumber("gyro", swerve.getGyroRotation3d().getZ());
 	}
 }
